62384a1af5400fbb554e8d0d43c92429bb225540
Anwell Matro <matro.aj@gmail.com>
2021-01-26 01:05:27 -0500

    Changed Router to use HashRouter instead (server side - gh pages - not configured to handle paths)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Link
